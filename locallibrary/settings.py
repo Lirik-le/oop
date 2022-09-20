5d4b7af4728a19453ecf02da3091d4d794cef352
Kirill <kirill.lebedev258@gmail.com>
2022-09-20 22:33:30 +0700

    pythonanywhere

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -53,8 +53,17 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'DB_NAME',
+
+        'USER': 'DB_USER',
+
+        'PASSWORD': 'DB_PASSWORD',
+
+        'HOST': 'localhost',
+
+        'PORT': '3306',
     }
 }
 
